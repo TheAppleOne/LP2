a8fe99f5f77633cfc72fd7455582205132b027ab
TheAppleOne <andrei.garbea@student.upt.ro>
2021-05-06 01:15:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import geopandas as gpd
 
 geo_df = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
 
-px.set_mapbox_access_token(open(".mapbox_token").read())
+px.set_mapbox_access_token(open("pk.eyJ1IjoidGhlYXBwbGVvbmUiLCJhIjoiY2tvYzBqYzljMWoybTJ2bXUzOW56Nm44YSJ9.g6ooCrImGr3EN337C8uY5Q").read())
 fig = px.scatter_mapbox(geo_df,
                         lat=geo_df.geometry.y,
                         lon=geo_df.geometry.x,
